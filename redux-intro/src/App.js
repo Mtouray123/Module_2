895ea97924564b183d9b5d03e75b4f9bdbd50985
Mahamadou Touray <mahamadou12@gmail.com>
2023-03-25 20:10:12 -0400

    redux-intro

---
+++ redux-intro/src/App.js
@@ -0,0 +1,15 @@
+import Counter from './components/Counter';
+import './App.css';
+import Todos from './components/Todos';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+      <hr/>
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
